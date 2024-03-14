ded5921c39cabb06a98a7f48fc7b7ad41a8bf07f
phddevelopment <155246606+Peacehomesdevelopment@users.noreply.github.com>
2024-03-15 01:50:13 +0400

    Add files via upload

--- doc/RELEASE_NOTES.md
+++ doc/RELEASE_NOTES.md
@@ -1,6 +1,6 @@
-## Module <cts_theme_rozz>
+## Module <theme_coffee_shop>
 
-#### 27.12.2023
+#### 08.03.2024
 #### Version 17.0.1.0.0
-##### ADD
-- Initial commit for Theme Rozz
+#### ADD
+- Initial commit for Theme Coffee Shop
--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
-################################################################################
+#############################################################################
 #
 #    Cybrosys Technologies Pvt. Ltd.
 #
-#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Ammu Raj (odoo@cybrosys.com)
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
 #
 #    You can modify it under the terms of the GNU LESSER
 #    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
@@ -19,13 +18,5 @@
 #    (LGPL v3) along with this program.
 #    If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
-from . import account_general_ledger
-from . import account_partner_ledger
-from . import account_trial_balance
-from . import aged_payable_report
-from . import aged_receivable_report
-from . import bank_book_report
-from . import cash_book_report
-from . import dynamic_balance_sheet_report
-from . import tax_report
+#############################################################################
+from . import theme_utils

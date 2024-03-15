b7dbe5b014e051249735e52e76139a202df7d279
Peacehomesdevelopment <155246606+Peacehomesdevelopment@users.noreply.github.com>
2024-03-15 14:56:42 +0400

    form and python

---
+++ formio_report_qweb-17.0.1.0/formio/models/formio_extra_asset.py
@@ -0,0 +1,9 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+from odoo import models
+
+
+class ExtraAsset(models.Model):
+    _name = 'formio.extra.asset'
+    _description = 'formio.js Extra Asset'
